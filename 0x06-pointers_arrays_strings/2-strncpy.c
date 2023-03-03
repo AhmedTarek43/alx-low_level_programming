18dcff3077498c0a0295fedfa24a01e6bc8f7965
AhmedTarek43 <agabry43@gmail.com>
2023-03-02 16:18:27 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,26 +1,30 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- *  *_strncpy - function that copies a string
- *   *
- *    * @src: The source of strings
- *     * @dest: The destination of the string
- *      * @n: The length of int
- *       *
- *        * Return: pointer to the resulting string dest
- *         */
+ *  * _strncpy - copies a string
+ *   * @dest: destination string
+ *    * @src: source string
+ *     * @n: number of bytes to copy
+ *      *
+ *       * Return: pointer to the resulting string
+ *        */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+		int i;
 
-		for (i = 0; i < n && *(src + i); i++)
-				{
-							*(dest + i) = *(src + i);
-								}
-			for (; i < n; i++)
-					{
-							*(dest + i) = '\0';
-								}
-				return (dest);
+			i = 0;
 
+				while (src[i] != '\0' && i < n)
+						{
+									dest[i] = src[i];
+											i++;
+												}
+
+					while (i < n)
+							{
+										dest[i] = '\0';
+												i++;
+													}
+
+						return (dest);
 }
